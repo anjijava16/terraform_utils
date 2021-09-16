678145a3b4b1f3008ae2c39b50733cb68d0879d2
Anjaiah Methuku <anjijava16@users.noreply.github.com>
2021-09-16 13:27:02 -0400

    Add files via upload

---
+++ gcp/gcs_storage/main.tf
@@ -0,0 +1,5 @@
+resource "google_storage_bucket" "COLDLINE"{
+name="akfkafkaprjectsss"
+storage_class= "COLDLINE"
+location="us-east1"
+}
---
+++ gcp/gcs_storage/provider.tf
@@ -0,0 +1,5 @@
+provider "google"{
+credentials= file("C:/Tech_Learn_welcome/Cloud_Tech_Learn/keys_clouds/Google/iwinner-data-318822-f14b3492fcfc.json")
+project ="iwinner-data-318822"
+region="us-east1"
+}
